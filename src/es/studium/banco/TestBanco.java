be2b99863499d0ef64bae1fa5c82d379f4bf940f
Coke-25 <franciscolopezk25@gmail.com>
2020-01-11 17:11:36 +0100

    Código refactorizado #2

--- src/es/studium/banco/TestBanco.java
+++ src/es/studium/banco/TestBanco.java
@@ -2,6 +2,12 @@ package es.studium.banco;
 
 public class TestBanco {
 	public static void main(String[] args) {
+		
+		final String enunciadoParte1 = "La cuenta de ";
+		final String enunciadoParte2 = " tiene ";
+		final String enunciadoParte3 = " euros.";
+		
+
 		/* Antonio y Beatriz se hacen cliente del banco */
 		Cliente antonio = new Cliente("123456789Z", "Antonio Alonso", "Av. Pueblo Saharaui, s/n");
 		Cliente beatriz = new Cliente("987654321A", "Beatriz Benítez", "Calle Sol, 4");
@@ -9,22 +15,19 @@ public class TestBanco {
 		/* Por defecto, todas las cuentas nuevas tienen 100€ */
 		Cuenta cuentaAntonio = new Cuenta(48151, 100, antonio);
 		Cuenta cuentaBeatriz = new Cuenta(62342, 100, beatriz);
+		
  
 		/* Antonio y Beatriz consultan el saldo */
-		System.out.println("La cuenta de " + cuentaAntonio.getNombreCliente().getNombre() + " tiene "
-				+ cuentaAntonio.getSaldo() + " euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getNombreCliente().getNombre() + " tiene "
-				+ cuentaBeatriz.getSaldo() + " euros.");
+		consultaAntonio(enunciadoParte1, enunciadoParte2, enunciadoParte3, cuentaAntonio);
+		consultaBeatriz(enunciadoParte1, enunciadoParte2, enunciadoParte3, cuentaBeatriz);
  
 		/* Beatriz transfiere 50€ a Antonio */
 		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() - 50);
 		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() + 50);
  
 		/* Antonio y Beatriz vuelven a consultar para comprobar que todo ha ido bien */
-		System.out.println("La cuenta de " + cuentaAntonio.getNombreCliente().getNombre() + " tiene "
-				+ cuentaAntonio.getSaldo() + " euros.");
-		System.out.println("La cuenta de " + cuentaBeatriz.getNombreCliente().getNombre() + " tiene "
-				+ cuentaBeatriz.getSaldo() + " euros.");
+		consultaAntonio(enunciadoParte1, enunciadoParte2, enunciadoParte3, cuentaAntonio);
+		consultaBeatriz(enunciadoParte1, enunciadoParte2, enunciadoParte3, cuentaBeatriz);
  
 		/* Antonio gana 100€ en una rifa y hace un ingreso en su cuenta */
 		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() + 100);
@@ -35,5 +38,15 @@ public class TestBanco {
 		/* Antonio transfiere 50€ a Beatriz */
 		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() - 50);
 		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() + 50);
+	}
+
+	private static void consultaBeatriz(final String enunciadoParte1, final String enunciadoParte2, final String enunciadoParte3, Cuenta cuentaBeatriz) 
+	{
+		System.out.println(enunciadoParte1 + cuentaBeatriz.getNombreCliente().getNombre() + enunciadoParte2 + cuentaBeatriz.getSaldo() + enunciadoParte3);
+	}
+
+	private static void consultaAntonio(final String enunciadoParte1, final String enunciadoParte2, final String enunciadoParte3, Cuenta cuentaAntonio) 
+	{
+		System.out.println(enunciadoParte1 + cuentaAntonio.getNombreCliente().getNombre() + enunciadoParte2 + cuentaAntonio.getSaldo() + enunciadoParte3);
 	}
 }
